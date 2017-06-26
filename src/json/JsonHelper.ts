b93922462724876db65cb1d12fcb6f5b24a43eb7
Alan Gomes <alanhr2@gmail.com>
2017-06-26 15:00:28 -0300

    fix(lint): add semicolon

--- src/json/JsonHelper.ts
+++ src/json/JsonHelper.ts
@@ -3,10 +3,10 @@ import { Jsonable } from "./Jsonable";
 
 export const fromObject = (jsonable: Jsonable): Object => {
   return jsonable.toJson();
-}
+};
 
 export const fromList = (list: Array<Jsonable>): Array<Object> => {
   return list.map((jsonable: Jsonable) => {
     return fromObject(jsonable);
   });
-}
+};
